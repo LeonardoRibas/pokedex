5d134d31b35abeaa3f38faaab79dd0a37d9d4134
brunofvteixeira <brunofvteixeira@gmail.com>
2020-05-26 00:30:23 -0300

    remove botao e titulo na pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 class Login extends React.Component {
+  usernameInput = React.createRef();
+
+  keyPressed = (e) => {
+    if (e.key === "Enter") {
+      let username = this.usernameInput.current.value;
+      this.usernameInput.current.value = "";
+      this.sendUsername(username);
+    }
+  };
+
+  sendUsername = (username) => {
+    // mandar username para endpoint de login na API
+  };
+
   render() {
     return (
       <div className="loginContainer">
-        <div>Pokedex</div>
-        <input type="text" placeholder="Username" />
-        <button>Login</button>
+        <input
+          type="text"
+          ref={this.usernameInput}
+          onKeyPress={this.keyPressed}
+          placeholder="Username"
+        />
       </div>
     );
   }
