0194d561226489d35314d22913ae9bd8131c8eec
brunofvteixeira <brunofvteixeira@gmail.com>
2020-05-27 01:03:54 -0300

    faz requisicoes para a api e armazena o usuario em localStorage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
 import React from "react";
+import axios from "axios";
+
+const User = React.createContext(null);
 
 class Login extends React.Component {
   usernameInput = React.createRef();
@@ -12,7 +15,18 @@ class Login extends React.Component {
   };
 
   sendUsername = (username) => {
-    // mandar username para endpoint de login na API
+    axios
+      .post("https://pokedex20201.herokuapp.com/users", { username: username })
+      .then((res) => {
+        if (res.status === 201) {
+          localStorage.setItem("user", username);
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 422) {
+          localStorage.setItem("user", username);
+        }
+      });
   };
 
   render() {
