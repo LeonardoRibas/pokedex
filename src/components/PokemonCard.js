8c30c4e1c37e1c910976b58cd19d27eaa3eef5dd
Léo Ribas <leoribasnasc@gmail.com>
2020-05-27 18:56:29 -0300

    Adiciona a função de favoritar um pokémon

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,13 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 import styles from "./PokemonCard.module.css";
 
 const PokemonCard = (props) => {
+  const handleFavorite = (pokemonName) => {
+    const userName = localStorage.getItem("user");
+    axios.post(
+      `https://pokedex20201.herokuapp.com/users/${userName}/starred/${pokemonName}`
+    );
+  };
+
   return (
     <div className={styles.card}>
       <h1>{props.name}</h1>
       <img src={props.img} alt={props.name} />
       <p>{props.kind}</p>
+      <button onClick={() => handleFavorite(props.name)}>Favoritar</button>
     </div>
   );
 };
