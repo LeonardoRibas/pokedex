54aa300423e3b07942496aabc4dc07a2765a12de
Léo Ribas <leoribasnasc@gmail.com>
2020-05-29 19:20:52 -0300

    Provider provêm os pokemons favoritos aos componentes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Provider from "./context/FavoritePokemonContext";
 
 import Login from "./pages/Login";
 import Catalog from "./pages/Catalog";
@@ -8,8 +9,10 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Login} exact />
-        <Route path="/catalog" component={Catalog} />
+        <Provider>
+          <Route path="/" component={Login} exact />
+          <Route path="/catalog" component={Catalog} />
+        </Provider>
       </Switch>
     </BrowserRouter>
   );
