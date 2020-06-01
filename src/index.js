2d53844d17af98a5ea469f0175638ed33d8ded28
Léo Ribas <leoribasnasc@gmail.com>
2020-06-01 19:53:21 -0300

    Cria o componente App q recebe estilização global

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Router from "./routes";
+
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
