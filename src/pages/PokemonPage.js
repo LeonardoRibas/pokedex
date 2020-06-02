0b8223f3e7b0a2ddb0fc0cd0573a4a765e0f3c84
Léo Ribas <leoribasnasc@gmail.com>
2020-06-01 23:00:56 -0300

    Estiliza a página de pokemon

--- src/pages/PokemonPage.js
+++ src/pages/PokemonPage.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
+
+import KindRenderer from "../components/KindRenderer";
+
 import { FavoritePokemonContext } from "../context/FavoritePokemonContext";
 
 import Navbar from "../components/Navbar";
+import { useHistory } from "react-router-dom";
+
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+
+import styles from "./PokemonPage.module.css";
 
 const PokemonPage = (props) => {
-  const [pokemon, setPokemon] = useState({ name: props.match.params.name });
-
   const username = localStorage.getItem("user");
+  const history = useHistory();
+
+  const [pokemon, setPokemon] = useState({ name: props.match.params.name });
 
   const { favoritePokemons, setFavoritePokemons } = useContext(
     FavoritePokemonContext
@@ -56,25 +65,48 @@ const PokemonPage = (props) => {
   };
 
   return (
-    <>
-      <Navbar />
-      <div className="pokemon">
-        <img src={pokemon.image_url} alt="" />
-        <div className="pokemonName">{pokemon.name}</div>
-        <div className="pokemonKind">{pokemon.kind}</div>
-        <div className="pokemonWeight">{pokemon.weight}</div>
-        <div className="pokemonHeight">{pokemon.height}</div>
-        {isFavorite ? (
-          <button onClick={() => handleUnfavorite(pokemon.name)}>
-            Desfavoritar
-          </button>
-        ) : (
-          <button onClick={() => handleFavorite(pokemon.name)}>
-            Favoritar
-          </button>
-        )}
+    <div className={styles.container}>
+      <div className={styles.navigation}>
+        <button
+          className={styles.backIcon}
+          onClick={() => history.push("/catalog")}
+        >
+          <ArrowBackIcon />
+        </button>
+        <Navbar />
       </div>
-    </>
+      <div className={styles.pokemonCard}>
+        <div className={styles.pokemonAvatar}>
+          <h1 className={styles.pokemonName}>{`N° ${pokemon.number}`}</h1>
+          <img className={styles.pokemonImage} src={pokemon.image_url} alt="" />
+        </div>
+
+        <div className={styles.description}>
+          <div className={styles.pokemonName}>{pokemon.name}</div>
+          {pokemon.kind ? (
+            <KindRenderer kind={pokemon.kind} big={true} />
+          ) : null}
+          <div className={styles.pokemonWeight}>{pokemon.weight}</div>
+          <div className="pokemonHeight">{pokemon.height}</div>
+
+          {isFavorite ? (
+            <button
+              className={styles.favButton}
+              onClick={() => handleUnfavorite(pokemon.name)}
+            >
+              Desfavoritar
+            </button>
+          ) : (
+            <button
+              className={styles.unfavButton}
+              onClick={() => handleFavorite(pokemon.name)}
+            >
+              Favoritar
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
