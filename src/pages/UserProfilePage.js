f94bfa73c06cd3fa85ab6e040738e475196c4bdf
brunofvteixeira <brunofvteixeira@gmail.com>
2020-06-01 21:44:00 -0300

    cria aviso de lista vazia na pagina de perfil do usuario

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 import PokemonCard from "../components/PokemonCard";
 import Navbar from "../components/Navbar";
@@ -28,20 +29,29 @@ const UserProfilePage = () => {
       <Navbar />
       <div className={styles.container}>
         <h1>Meus Pokémons Favoritos</h1>
-        <div className={styles.catalog}>
-          {favoritePokemons.map((favPokemon) => (
-            <PokemonCard
-              key={favPokemon.id}
-              id={favPokemon.id}
-              img={favPokemon.image_url}
-              name={favPokemon.name}
-              number={favPokemon.number}
-              kind={favPokemon.kind}
-              height={favPokemon.height}
-              weight={favPokemon.weight}
-            />
-          ))}
-        </div>
+        {favoritePokemons.length !== 0 ? (
+          <div className={styles.catalog}>
+            {favoritePokemons.map((favPokemon) => (
+              <PokemonCard
+                key={favPokemon.id}
+                id={favPokemon.id}
+                img={favPokemon.image_url}
+                name={favPokemon.name}
+                number={favPokemon.number}
+                kind={favPokemon.kind}
+                height={favPokemon.height}
+                weight={favPokemon.weight}
+              />
+            ))}
+          </div>
+        ) : (
+          <>
+            <h2 className={styles.emptyList}>Nenhum Pokémon Favorito</h2>
+            <Link to="/catalog" className={styles.catalogLink}>
+              Pokémons
+            </Link>
+          </>
+        )}
       </div>
     </>
   );
